dccdedcbab7df8208ca8f92aaa64c4e33ef7254c
KESHAVsCODE <106483384+KESHAVsCODE@users.noreply.github.com>
2023-11-20 00:59:57 +0530

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Splash from "./components/Splash";
+
+const App = () => {
+  return (
+    <div className="text-red-400">
+      <Splash />
+    </div>
+  );
+};
+export default App;
---
+++ src/components/Splash.jsx
@@ -0,0 +1,9 @@
+const Splash = () => {
+  return (
+    <section className=" text-sm font-dairyMilk ">
+      A unique birthday song everyone
+    </section>
+  );
+};
+
+export default Splash;
